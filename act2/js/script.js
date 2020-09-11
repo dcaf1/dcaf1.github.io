785b78df5d3bd1a629b74352b36bd4f13acb2ae0
Ivan HG <ai.hernandezgarcia@ugto.mx>
2020-09-11 13:34:19 -0500

    hora java

--- act2/js/script.js
+++ act2/js/script.js
@@ -1,8 +1,26 @@
-/*
-	Alpha by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
+function mostrarHora(){
+    momentoActual = new Date();
+    hora = momentoActual.getHours();
+    minuto = momentoActual.getMinutes();
+    segundo = momentoActual.getSeconds();
+
+    horaImprimible = hora + " : " + minuto + " : " + segundo;
+
+    document.getElementById('mostrarHora').innerHTML = horaImprimible;
+
+}
+
+function mostrarFecha(){
+    fechaActual = new Date();
+    mes = fechaActual.getUTCMonth() + 1;
+    dia = fechaActual.getUTCDate();
+    año = fechaActual.getUTCFullYear();
+
+    fechaImprime = dia + "/" + mes + "/" + año; 
+
+    document.getElementById('mostrarFecha').innerHTML = fechaImprime;
+}
 
 (function($) {
 
