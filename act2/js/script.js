5ada706b2aefa01371dc13b87286af72859e5f9d
Ivan HG <ai.hernandezgarcia@ugto.mx>
2020-09-19 10:50:29 -0500

    unidegx_act3

---
+++ act2/js/script.js
@@ -0,0 +1,29 @@
+
+
+function fecha(){
+    fecha = new Date();
+    mes = fecha.getUTCMonth() + 1;
+    dia = fecha.getUTCDate();
+    año = fecha.getUTCFullYear();
+
+    fechax = dia + "/" + mes + "/" + año; 
+
+    document.getElementById('fecha').innerHTML = fechax;
+}
+
+
+function hora(){
+    time = new Date();
+    hora = time.getHours();
+    minuto = time.getMinutes();
+    segundo = time.getSeconds();
+
+    timex = hora + ":" + minuto + ":" + segundo;
+
+    document.getElementById('hora').innerHTML = timex;
+
+}
+
+
+
+
